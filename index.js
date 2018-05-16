7598a4a33bd2d6982643c17fb4ad7fbd701d9749
Tomas <tomasholderness@gmail.com>
2018-05-16 14:15:00 -0400

    added lint

--- index.js
+++ index.js
@@ -9,41 +9,41 @@ exports.handler = function(event, context) {
     console.log('From SNS:', event.Records[0].Sns.Message);
 
     const postData = {
-        "channel": CHANNEL,
-        "username": "AWS SNS",
-        "text": "*" + event.Records[0].Sns.Subject + "*",
-        "icon_emoji": ":scream:"
+        'channel': CHANNEL,
+        'username': 'AWS SNS',
+        'text': '*' + event.Records[0].Sns.Subject + '*',
+        'icon_emoji': ':scream:',
     };
 
     const sns = JSON.parse(event.Records[0].Sns.Message);
-    let message = sns.AlaramDescription + '\n Region: ' + sns.Region + 
+    let message = sns.AlaramDescription + '\n Region: ' + sns.Region +
     '\n Timestamp: ' + sns.StateChangeTime;
 
     postData.attachments = [
         {
-            "color": "danger", 
-            "text": message
-        }
+            'color': 'danger',
+            'text': message,
+        },
     ];
 
     const options = {
         method: 'POST',
         hostname: 'hooks.slack.com',
         port: 443,
-        path: PATH
+        path: PATH,
     };
 
     const req = https.request(options, function(res) {
       res.setEncoding('utf8');
-      res.on('data', function (chunk) {
+      res.on('data', function(chunk) {
         context.done(null, postData);
       });
     });
-    
+
     req.on('error', function(e) {
       console.log('problem with request: ' + e.message);
-    });    
+    });
 
-    req.write(util.format("%j", postData));
+    req.write(util.format('%j', postData));
     req.end();
 };
